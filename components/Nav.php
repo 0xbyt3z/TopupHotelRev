908ace90e2a6caa8746b39695f69d8a3a6e0e0a7
theekshana <rtheekshanafernando@gmail.com>
2022-08-17 00:45:16 +0530

    add .htaccess

--- components/Nav.php
+++ components/Nav.php
@@ -11,8 +11,8 @@
 </script>
 
 <div class="h-16 w-screen bg-gray-700 flex items-center justify-end">
-    <a href="" class="text-white mx-3">Home</a>
-    <a href="" class="text-white mx-3">Signup</a>
+    <a href="/" class="text-white mx-3">Home</a>
+    <a href="cussingup.php" class="text-white mx-3">Signup</a>
     <div>
         <div class="rounded-full w-10 h-10 bg-white mr-10 ml-10" onclick="toggleAvatarDropDown();">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
