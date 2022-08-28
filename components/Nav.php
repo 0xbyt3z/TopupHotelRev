72d08ecf76c6624b25ae0655734856fd719e7ea5
Theekshana Fernando <40062006+0xbyt3z@users.noreply.github.com>
2022-08-28 11:03:07 +0530

    fix

--- components/Nav.php
+++ components/Nav.php
@@ -15,7 +15,7 @@
 
     <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
       <ul
-        class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white bg-gray-800 bg-gray-900 border-gray-700">
+      class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
           <a href="/topuphotelrev"
             class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
